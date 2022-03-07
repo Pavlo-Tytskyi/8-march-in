adc22a659f8424749c4ea53eb7e6c214be9727f0
Pasha <tickijpavlo@gmail.com>
2022-03-08 01:50:28 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/* https://greensock.com/gsap */
+TweenLite.set("#petals", { perspective:600 })
+TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+
+var total = 50;
+var warp = document.getElementById("petals"),
+    w = window.innerWidth,
+    h = window.innerHeight;
+
+for (i = 0; i < total; i++) {
+    var Div = document.createElement('div');
+    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    warp.appendChild(Div);
+    animm(Div);
+}
+
+function animm(elm) {
+    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
+    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
+    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+};
+
+function R(min,max) { return min+Math.random() * (max-min) };
+
+
+/* https://mattboldt.com/typed.js/ */
+var typed = new Typed('#text', {
+    strings: ['Я тебе люблю дуже-дуже сильно і буду любити завжди. Люблю тебе за твою підтримку, за твою красу, за твій милий характер, за твою скромність, за твої очі, за твою фігуру, за твій голос, за твою посмішку, за твою доброту, за твою щедрість, за те що довіряєш мені, за те що терпиш мене, за те що піклуєшся про мене, за те що завжди була чесною зі мною, за те що проводиш зі мною час, за те що розумієш мене і за те що любиш мене.'],
+    startDelay: 3000,
+    typeSpeed: 50,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    showCursor: false,
+    onComplete: function() {
+        var author = document.getElementById("author");
+        author.style.opacity = 1;
+    }
+});
